e92702d7fdda79037db97dc4eb26f8804743d3d1
saku <saku2saku@gmail.com>
2014-12-17 07:51:27 +0900

    初期状態のプロジェクトをコミット

---
+++ JavascriptTestSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JavascriptTestSample
+//
+//  Created by saku on 2014/12/17.
+//  Copyright (c) 2014年 Retty Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JavascriptTestSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JavascriptTestSample
+//
+//  Created by saku on 2014/12/17.
+//  Copyright (c) 2014年 Retty Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
