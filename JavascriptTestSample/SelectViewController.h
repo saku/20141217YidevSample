14c744880abe1ea490af1e13fcd77a75a6c21484
saku <saku2saku@gmail.com>
2014-12-17 08:07:29 +0900

    サンプルコードの選択画面を追加

---
+++ JavascriptTestSample/SelectViewController.h
@@ -0,0 +1,11 @@
+//
+//  SelectViewController.h
+//
+//  Copyright (c) 2014 Yoichiro Sakurai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectViewController : UITableViewController
+
+@end
---
+++ JavascriptTestSample/SelectViewController.m
@@ -0,0 +1,73 @@
+//
+//  SelectViewController.h
+//
+//  Copyright (c) 2014 Yoichiro Sakurai. All rights reserved.
+//
+
+#import "SelectViewController.h"
+
+@interface SelectViewController ()
+
+@property NSArray *samples;
+
+@end
+
+@implementation SelectViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.title = @"Sample Selection";
+    _samples = @[
+                 @"SimpleJavascript",
+                 @"HandlebarsSample",
+                 ];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _samples.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        cell.textLabel.adjustsFontSizeToFitWidth = YES;
+    }
+    
+    // Configure the cell...
+    cell.textLabel.text = _samples[indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    NSString *className = [NSString stringWithFormat:@"%@ViewController", _samples[indexPath.row]];
+    UIViewController *viewController = [[NSClassFromString(className) alloc] init];
+    viewController.title = _samples[indexPath.row];
+
+    [self.navigationController pushViewController:viewController animated:YES];
+}
+
+@end
